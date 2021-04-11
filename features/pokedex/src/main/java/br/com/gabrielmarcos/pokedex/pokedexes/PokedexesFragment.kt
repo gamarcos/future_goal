510cf2549569f09a1a033d3ddd3034287b5d857b
Gabriel Marcos <gabriel.marcos@luizalabs.com>
2021-04-11 18:08:16 -0300

    Improvement usecase strategy

--- features/pokedex/src/main/java/br/com/gabrielmarcos/pokedex/pokedexes/PokedexesFragment.kt
+++ features/pokedex/src/main/java/br/com/gabrielmarcos/pokedex/pokedexes/PokedexesFragment.kt
@@ -2,31 +2,22 @@ package br.com.gabrielmarcos.pokedex.pokedexes
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import br.com.gabrielmarcos.futuregoals.GoalApplication.Companion.coreComponent
 import br.com.gabrielmarcos.pokedex.R
 import br.com.gabrielmarcos.pokedex.di.DaggerPokedexComponent
-import br.com.gabrielmarcos.pokedex.pokedexes.domain.GetNextPokedexes
-import br.com.gabrielmarcos.pokedex.pokedexes.domain.GetPokedexes
-import br.com.gabrielmarcos.pokedex.pokedexes.domain.model.Pokedexes
+import kotlinx.android.synthetic.main.fragment_features_pokedex.*
 import javax.inject.Inject
 
 class PokedexesFragment : Fragment() {
 
     @Inject
-    lateinit var pokedexes: MutableLiveData<Pokedexes>
-
-    @Inject
-    lateinit var getPokedexes: GetPokedexes
-
-    @Inject
-    lateinit var getNextPokedexes: GetNextPokedexes
+    lateinit var viewModel: PokedexesViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -49,19 +40,19 @@ class PokedexesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getPokedexes()
+        viewModel.getNextPokedexes()
 
-        getPokedexes("")
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+            loading.visibility = if (it) View.VISIBLE else View.GONE
+        })
 
-        Handler().postDelayed({
-            getNextPokedexes("")
-        }, 3000)
+        viewModel.pokedexes.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it?.size.toString(), Toast.LENGTH_LONG).show()
+        })
 
-        Handler().postDelayed({
-            getNextPokedexes("")
-        }, 6000)
-
-        pokedexes.observe(viewLifecycleOwner, Observer {
-            println("TESTE_POKEDEX: pokedexes $it")
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
         })
     }
 }
--- features/pokedex/src/main/java/br/com/gabrielmarcos/pokedex/pokedexes/domain/GetNextPokedexes.kt
+++ features/pokedex/src/main/java/br/com/gabrielmarcos/pokedex/pokedexes/domain/GetNextPokedexes.kt
@@ -1,29 +1,32 @@
 package br.com.gabrielmarcos.pokedex.pokedexes.domain
 
-import androidx.lifecycle.MutableLiveData
 import br.com.gabrielmarcos.core.data.cache.ICache
 import br.com.gabrielmarcos.core.usecase.UseCase
 import br.com.gabrielmarcos.pokedex.pokedexes.data.PokedexRepository
-import br.com.gabrielmarcos.pokedex.pokedexes.domain.mapper.mapToPokedexes
+import br.com.gabrielmarcos.pokedex.pokedexes.data.model.PokedexResponse
 import br.com.gabrielmarcos.pokedex.pokedexes.domain.mapper.mapToPokedexesNavigation
-import br.com.gabrielmarcos.pokedex.pokedexes.domain.model.Pokedexes
 import br.com.gabrielmarcos.pokedex.pokedexes.domain.model.PokedexesNavigation
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.SendChannel
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class GetNextPokedexes @Inject constructor(
     private val repository: PokedexRepository,
-    private val liveData: MutableLiveData<Pokedexes>,
-    private val cache: ICache
-) : UseCase<String>() {
+    private val cache: ICache,
+    private val coroutineDispatcher: CoroutineDispatcher
+) : UseCase<Unit, List<PokedexResponse>?>(coroutineDispatcher) {
 
-    override suspend fun run(input: String) {
+    override suspend fun run(channel: SendChannel<List<PokedexResponse>?>, params: Unit?) {
         cache.get<PokedexesNavigation>(POKEDEXES_NAVIGATION)?.next?.takeIf {
             it.isNotBlank()
         }?.let { nextPage ->
             val result = repository.getPokedexesPage(nextPage)
             val navigation = result.mapToPokedexesNavigation()
             cache.put(POKEDEXES_NAVIGATION, navigation)
-            liveData.postValue(result.mapToPokedexes())
+            channel.send(result.results)
+            channel.close()
         }
     }
 }
